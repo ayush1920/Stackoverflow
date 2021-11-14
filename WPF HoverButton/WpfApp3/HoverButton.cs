31f10c4412293dab34b44396e9b65b7a1a7b6b02
Ayush Kumar <45363959+ayush1920@users.noreply.github.com>
2021-11-14 16:27:20 +0530

    initial commit

---
+++ WPF HoverButton/WpfApp3/HoverButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+namespace StackOverflow
+{
+    public class HoverButton : Button
+    {
+
+        public static readonly DependencyProperty hoverColorProperty = DependencyProperty.Register
+            (
+                 "hoverColor",
+                 typeof(SolidColorBrush),
+                 typeof(HoverButton),
+                 new PropertyMetadata(new BrushConverter().ConvertFrom("#5D5D5D"))
+            );
+
+        public SolidColorBrush hoverColor
+        {
+            get { return (SolidColorBrush)GetValue(bgColorProperty); }
+            set { SetValue(bgColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty bgColorProperty = DependencyProperty.Register
+         (
+              "bgColor",
+              typeof(SolidColorBrush),
+              typeof(HoverButton),
+              new PropertyMetadata(new SolidColorBrush(Colors.Red))
+         );
+
+        public SolidColorBrush bgColor
+        {
+            get { return (SolidColorBrush)GetValue(hoverColorProperty); }
+            set { SetValue(hoverColorProperty, value); }
+        }
+    }
+}
