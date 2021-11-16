601206947e9e9e079098176f1d624076d4a5a049
Ayush Kumar <45363959+ayush1920@users.noreply.github.com>
2021-11-16 17:43:01 +0530

    - Added CornerRadius and Border function

--- WPF HoverButton/WpfApp3/HoverButton.cs
+++ WPF HoverButton/WpfApp3/HoverButton.cs
@@ -38,5 +38,19 @@ namespace StackOverflow
             get { return (SolidColorBrush)GetValue(bgColorProperty); }
             set { SetValue(bgColorProperty, value); }
         }
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register
+      (
+           "CornerRadius",
+           typeof(CornerRadius),
+           typeof(HoverButton),
+           new PropertyMetadata(new CornerRadius())
+      );
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
     }
 }
